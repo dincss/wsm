d78e6a252f91354c68086337cdd9df9771d04717
Dennis Nesterovsky <deanfit@gmail.com>
2017-08-10 15:47:10 +0300

    correct dropdowns

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
   $('.sandwich').click(function(event) {
     $('.main_menu').toggleClass('js');
   });
+
+
+  $('.select_box').click(function(event) {
+  	$(this).next('.dropped').toggleClass('js');
+  });
     
 
 });
